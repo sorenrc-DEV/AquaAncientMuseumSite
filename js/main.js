c177cbc56fe1c2058187015c6a009f424d383b34
sorenrc-DEV <carley.sorensen@bellevuecollege.edu>
2026-02-05 00:49:44 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-// Placeholder for future interactive features
 console.log("AquaAncient Museum JS loaded");
 
-// Example: Alert welcome message
 window.onload = function() {
     console.log("Welcome to AquaAncient Museum!");
 };
